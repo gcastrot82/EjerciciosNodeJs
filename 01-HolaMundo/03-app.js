82c8c70f6d2314f59c8b8a325ae2a9df0b407157
Gerardo Castro Tamayo <gcastrot82@hotmail.com>
2022-03-23 18:58:09 -0500

    Update: Agregar nuevos directorios

---
+++ 01-HolaMundo/03-app.js
@@ -0,0 +1,19 @@
+console.log('Inicio de programa');
+
+
+setTimeout( () => {
+    console.log('Primer TimeOut');
+},3000);
+
+setTimeout( () => {
+    console.log('Segundo TimeOut');
+},0);
+
+
+setTimeout( () => {
+    console.log('Tercer Time Out');
+},0);
+
+
+
+console.log('Fin de programa');
