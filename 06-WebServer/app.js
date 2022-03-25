cd16a43f8a36266724d8afc18ca2b4ed625ef417
Gerardo Castro Tamayo <gcastrot82@hotmail.com>
2022-03-25 10:15:36 -0500

    Seccion8: Udemy NodeJs

---
+++ 06-WebServer/app.js
@@ -0,0 +1,33 @@
+
+const express = require('express')
+const app = express()
+const port=3000;
+
+
+// Ejecutar MidelWare
+
+// Servir contenido estatico, definri permisos sobre el directorio
+app.use ( express.static('public'));
+
+// Estas lineas ya no se usa:
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// });
+
+app.get('/hola-mundo', (req,res) => {
+    res.send('Hola cali!!!')
+});
+
+app.get('*',(req,res)=>{
+    
+    // Linea para mostrar string
+    //res.send("<h1>404 | Page not Found</h1>")
+
+    // Linea para cargar una pagina
+    res.sendFile(__dirname + '/public/msg_server/404.html')
+})
+
+// Se agregó un callback
+app.listen(port,()=>{
+    console.log(`Servicio ejecutandose en el puerto ${port}`);
+})
